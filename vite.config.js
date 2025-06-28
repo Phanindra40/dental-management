fe784618bbd90c89b35af6a485e8e155e52f9f74
chintha phanindra <chintha.phanindra40@gmail.com>
2025-06-28 22:01:17 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/",
+  base:"/dental-management",
   build: {
     outDir: 'docs',
   },
